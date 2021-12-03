7d1e831f6faf906ba8211c0072d44dc4d98fd578
CWCTBOY <qkrdlswo98@gmail.com>
2021-12-03 21:04:32 +0900

    hour

--- app.js
+++ app.js
@@ -11,16 +11,16 @@ const hourDeg = ;
 
 function rotatePointer() {
   const date = Date.now();
-  // const hour = (date / (1000 * 60 * 60) * 2) % 24;
+  const hour = (date / (1000 * 60 * 60)) % 24 - 3;
   const min = (date / (1000 * 60)) % 60;
   const sec = (date / 1000) % 60;
-  // const hourDeg = hour * 30;
+  const hourDeg = hour * 30;
   const minDeg = min * 6;
   const secDeg = sec * 6;
-  //  pointHour.style.transform = `rotate(${hourDeg}deg)`;
+  pointHour.style.transform = `rotate(${hourDeg}deg)`;
   pointMin.style.transform = `rotate(${minDeg}deg)`;
   pointSec.style.transform = `rotate(${secDeg}deg)`;
-  console.log(hour);
+  console.log((Date.now() / (1000 * 60 * 60)) % 24 - 3);
 }
-setInterval(rotatePointer, 1);
+setInterval(rotatePointer, 0.00001);
 

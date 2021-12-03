2c9d41b0faa8399f279172b192dba506823e909b
CWCTBOY <qkrdlswo98@gmail.com>
2021-12-03 19:03:19 +0900

    animation

--- app.js
+++ app.js
@@ -7,16 +7,20 @@ const pointSec = document.querySelector(".pointer-box__s");
 
 /*
 const hourDeg = ;
- */
+*/
+
 function rotatePointer() {
   const date = Date.now();
-  const hour = (date / (1000 * 60 * 60)) % 24;
+  // const hour = (date / (1000 * 60 * 60) * 2) % 24;
   const min = (date / (1000 * 60)) % 60;
   const sec = (date / 1000) % 60;
+  // const hourDeg = hour * 30;
   const minDeg = min * 6;
   const secDeg = sec * 6;
+  //  pointHour.style.transform = `rotate(${hourDeg}deg)`;
   pointMin.style.transform = `rotate(${minDeg}deg)`;
   pointSec.style.transform = `rotate(${secDeg}deg)`;
+  console.log(hour);
 }
 setInterval(rotatePointer, 1);
 

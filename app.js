8aaf56fbf1b1feeea20da8ad55eb567bdf178ff2
CWCTBOY <qkrdlswo98@gmail.com>
2021-12-03 17:57:26 +0900

    min,sec

---
+++ app.js
@@ -0,0 +1,22 @@
+const frame = document.querySelector(".clock-frame");
+
+const innerFace = document.querySelector(".clock-innerface");
+const pointHour = document.querySelector(".pointer-box__h");
+const pointMin = document.querySelector(".pointer-box__m");
+const pointSec = document.querySelector(".pointer-box__s");
+
+/*
+const hourDeg = ;
+ */
+function rotatePointer() {
+  const date = Date.now();
+  const hour = (date / (1000 * 60 * 60)) % 24;
+  const min = (date / (1000 * 60)) % 60;
+  const sec = (date / 1000) % 60;
+  const minDeg = min * 6;
+  const secDeg = sec * 6;
+  pointMin.style.transform = `rotate(${minDeg}deg)`;
+  pointSec.style.transform = `rotate(${secDeg}deg)`;
+}
+setInterval(rotatePointer, 1);
+
